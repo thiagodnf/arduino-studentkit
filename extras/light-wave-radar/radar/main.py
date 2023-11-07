7edf2a8e1c8b7b7cff73279afedfd867014f498b
Thiago Ferreira <thiagodnf@gmail.com>
2023-11-07 13:21:51 -0500

    added callback

--- extras/light-wave-radar/radar/main.py
+++ extras/light-wave-radar/radar/main.py
@@ -2,8 +2,13 @@
 from SerialPort import SerialPort
 from Radar import Radar
 
+def callback(args):
+    print(args)
+
 if __name__ == "__main__":
 
     radar = Radar()
+
+    radar.on("needleMoved", callback)
 
     radar.show()
--- extras/light-wave-radar/radar/radar.py
+++ extras/light-wave-radar/radar/radar.py
@@ -6,12 +6,15 @@ class Radar:
 
     def __init__(self) -> None:
 
+        self.events = {}
         self.winHeight = 300
         self.winWidth = 600
 
+        self.radarSize = 500
+
         self.refreshInterval = 10
 
-        self.needleAngle = 0
+        self.needleAngle = 45
         self.needleSpeed = 10
         self.needleDirection = 1
 
@@ -40,36 +43,28 @@ class Radar:
         # self.root.geometry("{}x{}+{}+{}".format(self.winWidth+20, self.winHeight+20, x_cordinate, y_cordinate))
         self.root.geometry("{}x{}".format(self.winWidth+20, self.winHeight+20))
 
+    def getBottomDimensions(self):
+        cx = self.winWidth / 2
+        cy = self.winHeight - 20
+        return cx, cy
+
+    def getDestination(self, x, y, angle, radius):
+
+        angle = (angle+90) * math.pi / 180
+
+        endX   = x + radius * math.sin(angle)
+        endY   = y + radius * math.cos(angle)
+
+        return endX, endY
+
     def drawRadar(self):
 
-        pad = 20
-        x = 400
+        for x in range(self.radarSize, 0, -100):
+            self.drawArcFromBottom(x, x)
 
-        width = 50
-        height = 50
-        cx = self.winWidth / 2
-        cy = self.winHeight
-
-        self.drawRec(cx-width/2, cy-height, width+(cx-width/2), cy)
-        # self.drawArc(cx, 0, 200, 200)
-
-
-        # for dist in range(0, x, 20):
-        # coord = 50, 50, (275*2), (300*2)
-        # # coord = 5+pad, 5+pad, 300*2-pad, 300*2-pad
-        # self.myCanvas.create_arc(coord, start=0, extent=180, fill="#2E6B6F", style=tkinter.CHORD)
-
-        # coord = 150, 150, 600,
-        # # coord = 5+pad, 5+pad, 300*2-pad, 300*2-pad
-        # self.myCanvas.create_arc(coord, start=0, extent=180, fill="#2E6B6F", width=2)
-
-        # draw arcs
-        # for dist in range(0, x, 20):
-        #     self.myCanvas.create_arc(dist+20, dist+20, (x*2)-dist-10, (x*2)-dist-10, start=0, extent=180, fill="#2E6B6F")
-
-        # for x in range(0, 180, 20):
-        #     startX, startY, endX, endY = self.getDestination(x)
-        #     self.drawLine(startX, startY, endX, endY, "white", 1)
+        for x in range(0, 180+1, 30):
+            self.drawLineFromBottom(x, self.radarSize / 2, "white", 1)
+            self.drawTextFromBottom(x, self.radarSize / 2, str(x)+u"\u00b0")
 
     def drawArc(self, x0, y0, x1, y1):
         self.myCanvas.create_arc(x0, y0, x1, y1, start=0, width=0, extent=180, fill="#2E6B6F", style=tkinter.CHORD)
@@ -79,32 +74,48 @@ class Radar:
         y = y - (radius/2)
         self.myCanvas.create_oval(x+radius, y, x, y+radius, fill="white", width=2)
 
-    def drawLine(self, startX, startY, endX, endY, fill, width):
+    def drawLine(self, startX, startY, endX, endY, fill="red", width=1):
         self.myCanvas.create_line(startX, startY, endX, endY, fill=fill, width=width)
 
     def drawRec(self, x0, y0, x1, y1):
-        self.myCanvas.create_rectangle(x0, y0, x1, y1, fill="blue", outline = 'blue')
+        self.myCanvas.create_rectangle(x0, y0, x1, y1, fill="blue", outline = 'red')
+
+    def drawText(self, x=10, y=10, text="", angle=0, font="Times 10"):
+        self.myCanvas.create_text(x, y, fill="white", font=font, text=text, angle=angle)
+
+    def drawArcFromBottom(self, width, height):
+
+        cx, cy = self.getBottomDimensions()
+
+        self.drawArc(cx-width/2, cy-height/2, width+(cx-width/2), cy+height/2)
+
+    def drawRecFromBottom(self, width=50, height=50):
+
+        cx, cy = self.getBottomDimensions()
+
+        self.drawRec(cx-width/2, cy-height, width+(cx-width/2), cy)
+
+    def drawLineFromBottom(self, angle, radius, fill="red", width=1):
+
+        cx, cy = self.getBottomDimensions()
+
+        endX, endY = self.getDestination(cx, cy, angle, radius)
+
+        self.drawLine(cx, cy, endX, endY, fill, width)
+
+    def drawTextFromBottom(self, angle, radius, text):
+
+        cx, cy = self.getBottomDimensions()
+
+        endX, endY = self.getDestination(cx, cy, angle, radius+10)
+
+        self.drawText(endX, endY, text, 0-(90-angle))
 
     def clearCanvas(self):
         self.myCanvas.delete('all')
 
-    def getDestination(self, angle, radius= 300):
-
-        x = 300
-        y = 300
-
-        angle = (angle+90) * math.pi / 180
-
-        startX = x
-        startY = y
-        endX   = x + radius * math.sin(angle)
-        endY   = y + radius * math.cos(angle)
-
-        return startX, startY, endX, endY
-
     def drawNeedle(self):
-        startX, startY, endX, endY = self.getDestination(self.needleAngle)
-        self.drawLine(startX, startY, endX, endY, "red", 3)
+        self.drawLineFromBottom(self.needleAngle, self.radarSize / 2)
 
     def update(self):
 
@@ -112,7 +123,7 @@ class Radar:
 
         self.drawRadar()
 
-        # self.drawNeedle()
+        self.drawNeedle()
 
         self.root.after(self.refreshInterval, self.update)
 
@@ -120,9 +131,11 @@ class Radar:
 
         self.needleAngle += self.needleDirection
 
-        if self.needleAngle > 180:
+        self.trigger("needleMoved", self.needleAngle)
+
+        if self.needleAngle >= 180:
             self.needleDirection = -1
-        if self.needleAngle < 0:
+        if self.needleAngle <= 0:
             self.needleDirection = 1
 
         self.root.after(self.needleSpeed, self.moveNeedle)
@@ -131,3 +144,14 @@ class Radar:
         self.root.after(self.refreshInterval, self.update)
         self.root.after(self.needleSpeed, self.moveNeedle)
         self.root.mainloop()
+
+    def on(self, eventName, callback):
+
+        if not eventName in self.events.keys():
+            self.events[eventName] = []
+
+        self.events[eventName].append(callback)
+
+    def trigger(self, eventName, args):
+        for callback in self.events[eventName]:
+            callback(args)
